44cb8941a4c6f965ec62637ef57415ba5d5c318f
Aleksandr Zorin <plazix@gmail.com>
2011-05-20 17:08:12 +0400

    добавлен setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-private-messages',
+    version='0.2.0',
+    description=u'Личные сообщения пользователей.',
+    author='Aleksandr Zorin (plazix)',
+    author_email='plazix@gmail.com',
+    url='https://github.com/plazix/django-private-messages',
+    download_url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
